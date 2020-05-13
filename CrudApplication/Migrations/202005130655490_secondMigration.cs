a3bc8c240ac57743daca8f1ab1d01c753bcec49b
siva4039 <marthasivakumar@gmail.com>
2020-05-13 14:15:06 +0530

    Crud Operations done

---
+++ CrudApplication/Migrations/202005130655490_secondMigration.cs
@@ -0,0 +1,16 @@
+namespace CrudApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class secondMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
